800aa9891492c7b2c688c7fa2d9327d73ffecec7
spennington <spennington13@gmail.com>
2020-12-14 16:26:41 -0500

    ETL
    update

--- DB Objects/Formula1.sql
+++ DB Objects/Formula1.sql
@@ -23,9 +23,7 @@ CREATE TABLE "ConstructorStandings" (
     "position" int   NOT NULL,
     "positionText" character varying(200)   NOT NULL,
     "wins" int   NOT NULL,
-    CONSTRAINT "pk_ConstructorStandings" PRIMARY KEY (
-        "constructorStandingsId"
-     )
+    PRIMARY KEY ("constructorStandingsId")
 );
 
 CREATE TABLE "ConstructorResults" (
@@ -33,37 +31,33 @@ CREATE TABLE "ConstructorResults" (
     "raceId" int   NOT NULL,
     "constructorId" int   NOT NULL,
     "points" int   NOT NULL,
-    "status" character varying(50)   NOT NULL,
-    CONSTRAINT "pk_ConstructorResults" PRIMARY KEY (
-        "constructorResultsId"
-     )
+    PRIMARY KEY ("constructorResultsId")
 );
 
 -- Table documentation comment 1 (try the PDF/RTF export)
 -- Table documentation comment 2
 CREATE TABLE "Race" (
-    "raceID" int   NOT NULL,
+    "raceId" int   NOT NULL,
     "year" int   NOT NULL,
     "round" int   NOT NULL,
     "circuitId" int   NOT NULL,
     "name" character varying(200)    NOT NULL,
     "date" Date   NOT NULL,
-    "time" time   NOT NULL,
+    "time" time,
     "url" character varying(2000)   NOT NULL,
-    CONSTRAINT "pk_Race" PRIMARY KEY (
-        "raceID"
-     )
+    PRIMARY KEY ("raceId")
+    
 );
 
 ALTER TABLE "ConstructorStandings" ADD CONSTRAINT "fk_ConstructorStandings_constructorId" FOREIGN KEY("constructorId")
 REFERENCES "Constructor" ("constructorId");
 
 ALTER TABLE "ConstructorResults" ADD CONSTRAINT "fk_ConstructorResults_raceId" FOREIGN KEY("raceId")
-REFERENCES "Race" ("raceID");
+REFERENCES "Race" ("raceId");
 
 ALTER TABLE "ConstructorResults" ADD CONSTRAINT "fk_ConstructorResults_constructorId" FOREIGN KEY("constructorId")
 REFERENCES "Constructor" ("constructorId");
 
+
 CREATE INDEX "idx_Constructor_constructorRef"
 ON "Constructor" ("constructorRef");
-
